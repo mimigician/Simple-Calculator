66f717d7707e345873f1b2dfd0fe3e1b028e336d
Mahmudul Hasan <rakib34216@gmail.com>
2022-05-10 21:46:00 +0600

    New_class added

---
+++ app/src/main/kotlin/new_package/new_class.java
@@ -0,0 +1,27 @@
+package new_package;
+
+public class new_class {
+    int a;
+    String s;
+    new_class(int ab, String sh)
+    {
+        a = ab;
+        sh = s;
+    }
+    public int get_a()
+    {
+        return a;
+    }
+    public String get_s()
+    {
+        return s;
+    }
+    public void set_a(int ab)
+    {
+        a =ab;
+    }
+    public void set_s(String sh)
+    {
+        s = sh;
+    }
+}
