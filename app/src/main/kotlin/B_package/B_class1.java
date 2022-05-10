76d9d233e4cadc4c5875f3bd5f03597f7fd451d4
Mahmudul Hasan <rakib34216@gmail.com>
2022-05-10 22:18:00 +0600

    Modified branch from branch2

--- app/src/main/kotlin/B_package/B_class1.java
+++ app/src/main/kotlin/B_package/B_class1.java
@@ -3,4 +3,11 @@ package B_package;
 public class B_class1 {
     int id;
     String name;
+    float cgpa;
+
+    public B_class1(int id, String name, float cgpa) {
+        this.id = id;
+        this.name = name;
+        this.cgpa = cgpa;
+    }
 }
