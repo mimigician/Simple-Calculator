14ac79cce78018edd26edc7b4ffc4116464c4d45
Mahmudul Hasan <rakib34216@gmail.com>
2022-05-10 22:22:44 +0600

    modified branch from main

--- app/src/main/kotlin/B_package/B_class1.java
+++ app/src/main/kotlin/B_package/B_class1.java
@@ -3,4 +3,11 @@ package B_package;
 public class B_class1 {
     int id;
     String name;
+    String father_name;
+
+    public B_class1(int id, String name, String father_name) {
+        this.id = id;
+        this.name = name;
+        this.father_name = father_name;
+    }
 }
